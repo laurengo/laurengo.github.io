baa4e0e8a5d66be8e747c666232357e493331b30
laurengo <lauren.gordon87@gmail.com>
2015-11-22 11:41:06 -0500

    classwork

---
+++ my-class-work/lifetime-supply/js/project.js
@@ -0,0 +1,15 @@
+document.getElementById('click-me').onclick = calculateLifetimeDrinks;
+
+function calculateLifetimeDrinks () {
+
+	var currentAge = document.getElementById('age').value;
+	var plannedAge = document.getElementById ('max-age').value;
+	var favDrink = document.getElementById ('item').value;
+	var numPerDay = document.getElementById ('num-per-day').value;
+
+	var lifetimeDrinks = (plannedAge - currentAge) * 365 * numPerDay;
+	console.log(lifetimeDrinks);
+
+	document.getElementById('solution').innerHTML = lifetimeDrinks;
+	document.getElementById('drink').innerHTML = favDrink;
+}
