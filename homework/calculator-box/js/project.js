4963e0e8b66d5eae6394e319e8ffdd3c31ccab99
laurengo <lauren.gordon87@gmail.com>
2015-12-01 11:16:24 -0500

    hw 12.1

---
+++ homework/calculator-box/js/project.js
@@ -0,0 +1,66 @@
+$(document).ready(function () {
+
+	var total = 0
+
+	// Add 10 Event
+	$('#a10').click(add10)
+	
+	function add10 () {
+		total = total + 10 
+		$('#out').text(total)
+	}
+
+	//Add 20 Event
+	$('#a20').click(add20)
+	
+	function add20 () {
+		total = total + 20
+		$('#out').text(total)
+	}
+
+	//Add 30 Event
+	$('#a30').click(add30)
+
+	function add30 () {
+		total = total + 30
+		$('#out').text(total)
+	}
+
+	//Make Red Event
+	$('#red').click(red)
+
+	function red () {
+		$("#out").css('background-color', 'red')
+	}
+
+	//Make Blue Event
+	$('#blue').click(blue)
+
+	function blue () {
+		$("#out").css('background-color', 'blue')
+	}
+
+	//Sub 30 Event
+	$('#n30').click(sub30)
+
+	function sub30 () {
+		total = total - 30
+		$('#out').text(total)
+	}
+
+	//Sub 20 Event
+	$('#n20').click(sub20)
+
+	function sub20 () {
+		total = total - 20
+		$('#out').text(total)
+	}
+
+	//Sub 10 Event
+	$('#n10').click(sub10)
+
+	function sub10 () {
+		total = total - 10
+		$('#out').text(total)
+	}
+})
---
+++ my-class-work/fortune-teller/js/project.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+
+	//document.getElementById('clickme').onclick = getFortune;
+	$('#clickme').click(getFortune)
+	function getFortune () {
+		// 1: Get your inputs!
+		var hometown = $('#hometown').val();
+		var partner = $('#partner').val();
+		var numKids = $('#numKids').val();
+		var jobTitle = $('#jobTitle').val();
+		//2: Run your func logic
+		var fortuneStr = "You will be a " + jobTitle + " living in " + hometown + " married to " + partner + " with " + numKids + " kids."
+
+		//3: Output
+		$('#output').text(fortuneStr)	
+	}
+})
